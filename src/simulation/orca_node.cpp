e570bab602989ddce31aad52bc8aaff7a2b711d4
zhefan <zhefanx@andrew.cmu.edu>
2022-05-15 23:56:32 -0400

    debug

--- src/simulation/orca_node.cpp
+++ src/simulation/orca_node.cpp
@@ -30,6 +30,34 @@ int main(int argc, char** argv){
 		op.makePlan(outputVel, true);
 		qm.setVelocity(outputVel[0], outputVel[1], 0);
 		cout << "Vel: " << outputVel[0] <<  " " << outputVel[1] << endl;
+
+		if (std::abs(outputVel[0]) >= 100){ // debug
+			cout << "============================debug========================" << endl;
+			// Robot Position and Goal position
+			nav_msgs::Odometry robotOdom = op.getOdom();
+			cout << "Current Position: " << robotOdom.pose.pose.position.x << ", " << robotOdom.pose.pose.position.y << endl;
+			cout << "GOal Position: " << goal[0] << ", " << goal[1] << endl;
+
+
+			// prefered vel in world frame
+			Eigen::Vector2d velPref = op.getVelPreferVec();
+			cout << "prefered velocity: " << velPref[0] << ", " << velPref[1] << endl;
+
+			// each obstacle position and information
+			std::vector<onboard_vision::Obstacle> obstacleVec;
+			onboard_vision::ObstacleList obMsg= op.getObstacles();
+			obstacleVec = obMsg.obstacles;
+			int count = 0;
+			for (onboard_vision::Obstacle ob : obstacleVec){
+				cout << "Obstacle " << count << ": position: " << "(" << ob.px  << ", " << ob.py << "). Vel: " << "(" << ob.vx << ", " << ob.vy << ")." << endl; 
+				cout << "size: " << std::max(ob.xsize, ob.ysize) << endl;
+				++count;  
+			}
+
+
+			break; 
+		}
+
 		ros::spinOnce();
 		r.sleep();
 	}
