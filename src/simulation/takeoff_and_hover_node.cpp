73ae536af78c4c7b11f240f911a23ea2bc8d9e63
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2022-10-11 14:31:10 -0400

    clean files and add takeoff

---
+++ src/simulation/takeoff_and_hover_node.cpp
@@ -0,0 +1,18 @@
+/*
+	FILE: takeoff_and_hover_node.cpp
+	-----------------------------
+	navigation ROS node
+*/
+
+#include <ros/ros.h>
+#include <autonomous_flight/simulation/flightBase.h>
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "takeoff_and_hover_node");
+	ros::NodeHandle nh;
+
+	AutoFlight::flightBase fb (nh);
+	fb.takeoff();
+	ros::spin();
+	return 0;
+}
