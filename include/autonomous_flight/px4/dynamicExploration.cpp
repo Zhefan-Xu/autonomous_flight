7ccc5558e5169330568a92a666ff71aa3b88fbdb
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2023-06-27 12:07:20 -0400

    implement exploration

--- include/autonomous_flight/px4/dynamicExploration.cpp
+++ include/autonomous_flight/px4/dynamicExploration.cpp
@@ -8,10 +8,69 @@
 
 namespace AutoFlight{
 	dynamicExploration::dynamicExploration(const ros::NodeHandle& nh) : flightBase(nh){
+		this->initParam();
+		this->initModules();
+	}
+
+	void dynamicExploration::initParam(){
+		// desired velocity
+		if (not this->nh_.getParam("autonomous_flight/desired_velocity", this->desiredVel_)){
+			this->desiredVel_ = 0.5;
+			cout << "[AutoFlight]: No desired velocity param. Use default 0.5m/s." << endl;
+		}
+		else{
+			cout << "[AutoFlight]: Desired velocity is set to: " << this->desiredVel_ << "m/s." << endl;
+		}	
+
+		// desired acceleration
+		if (not this->nh_.getParam("autonomous_flight/desired_acceleration", this->desiredAcc_)){
+			this->desiredAcc_ = 2.0;
+			cout << "[AutoFlight]: No desired acceleration param. Use default 2.0m/s^2." << endl;
+		}
+		else{
+			cout << "[AutoFlight]: Desired acceleration is set to: " << this->desiredAcc_ << "m/s^2." << endl;
+		}	
+
+		//  desired angular velocity
+		if (not this->nh_.getParam("autonomous_flight/desired_angular_velocity", this->desiredAngularVel_)){
+			this->desiredAngularVel_ = 0.5;
+			cout << "[AutoFlight]: No angular velocity param. Use default 0.5rad/s." << endl;
+		}
+		else{
+			cout << "[AutoFlight]: Angular velocity is set to: " << this->desiredAngularVel_ << "rad/s." << endl;
+		}	
+	}
+
+	void dynamicExploration::initModules(){
+		// initialize map
+		this->map_.reset(new mapManager::dynamicMap (this->nh_));
+
+		// initialize exploration planner
+		this->expPlanner_.reset(new globalPlanner::DEP (this->nh_));
+		this->expPlanner_->setMap(this->map_);
+
+		// initialize polynomial trajectory planner
+		this->polyTraj_.reset(new trajPlanner::polyTrajOccMap (this->nh_));
+		this->polyTraj_->setMap(this->map_);
+		this->polyTraj_->updateDesiredVel(this->desiredVel_);
+		this->polyTraj_->updateDesiredAcc(this->desiredAcc_);
+
+		// initialize piecewise linear trajectory planner
+		this->pwlTraj_.reset(new trajPlanner::pwlTraj (this->nh_));
+
+		// initialize bspline trajectory planner
+		this->bsplineTraj_.reset(new trajPlanner::bsplineTraj (this->nh_));
+		this->bsplineTraj_->setMap(this->map_);
+		this->bsplineTraj_->updateMaxVel(this->desiredVel_);
+		this->bsplineTraj_->updateMaxAcc(this->desiredAcc_);		
+	}
+
+	void dynamicExploration::registerCallback(){
 
 	}
 
 	void dynamicExploration::run(){
-
+		this->takeoff();
+		this->registerCallback();
 	}
 }
--- include/autonomous_flight/px4/dynamicExploration.h
+++ include/autonomous_flight/px4/dynamicExploration.h
@@ -6,16 +6,34 @@
 #ifndef DYNAMIC_EXPLORATION
 #define DYNAMIC_EXPLORATION
 #include <autonomous_flight/px4/flightBase.h>
-
+#include <global_planner/dep.h>
+#include <trajectory_planner/polyTrajOccMap.h>
+#include <trajectory_planner/piecewiseLinearTraj.h>
+#include <trajectory_planner/bsplineTraj.h>
+#include <map_manager/dynamicMap.h>
 
 namespace AutoFlight{
 	class dynamicExploration : flightBase{
 	private:
 		ros::NodeHandle nh_;
+		std::shared_ptr<mapManager::dynamicMap> map_;
+		std::shared_ptr<globalPlanner::DEP> expPlanner_;
+		std::shared_ptr<trajPlanner::polyTrajOccMap> polyTraj_;
+		std::shared_ptr<trajPlanner::pwlTraj> pwlTraj_;
+		std::shared_ptr<trajPlanner::bsplineTraj> bsplineTraj_;
+
+		// parameters
+		double desiredVel_;
+		double desiredAcc_;
+		double desiredAngularVel_;
 	
 	public:
 		dynamicExploration();
 		dynamicExploration(const ros::NodeHandle& nh);
+
+		void initParam();
+		void initModules();
+		void registerCallback();
 
 		void run();
 	};
